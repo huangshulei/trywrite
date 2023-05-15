d9abc51faf1cd0ee9b4c5b146dd1b241deea1893
shulei <2231778644>
2023-05-15 22:04:55 +0800

    two commit

--- src/main/java/com/example/trywrite/TrywriteApplication.java
+++ src/main/java/com/example/trywrite/TrywriteApplication.java
@@ -1,13 +1,19 @@
 package com.example.trywrite;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class TrywriteApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(TrywriteApplication.class, args);
+
+        log.info("项目启动成功");
     }
 
 }
